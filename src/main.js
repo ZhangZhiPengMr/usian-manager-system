4d5e69ed8c38dd5fd448d35a4e574f0264b14f71
ZhangZhiPengMr <3067673423@qq.com>
2022-09-19 20:03:23 +0800

    style(custom): 更改了项目icon图标和title标题

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-//引入ElementUI组件
 import store from "./store";
+//引入ElementUI组件
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false;
